0330434b01260a12bc6726761e73fade91473206
thipparaj <thipparaj@gmail.com>
2018-06-16 03:41:16 +0530

    Test Base initial browser launch using web driver

--- practice/src/main/java/testScript/practice/testBase/TestBase.java
+++ practice/src/main/java/testScript/practice/testBase/TestBase.java
@@ -1,13 +1,43 @@
 package testScript.practice.testBase;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class TestBase {
 	
 	public WebDriver driver;
 	
 
-	public void getBrowser(String myBrowser) {
+	public String getBrowser(String myBrowser) {
 		
+		if(System.getProperty("os.name").contains("Windows")) {
+			
+			if(myBrowser.equalsIgnoreCase("Chrome")) {
+			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/drivers/chromedriver.exe");
+			driver = new ChromeDriver();
+		}
+			else if(myBrowser.equalsIgnoreCase("Firefox")) {
+				System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"/drivers/geckodriver.exe");
+				driver = new FirefoxDriver();
+			}
+		}
+		else if(System.getProperty("os.name").contains("Mac")) {
+			
+			if(myBrowser.equalsIgnoreCase("Chrome")) {
+			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"/drivers/chromedriver.exe");
+			driver = new ChromeDriver();
+		}
+			else if(myBrowser.equalsIgnoreCase("Firefox")) {
+				System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"/drivers/geckodriver.exe");
+				driver = new FirefoxDriver();
+			}
+		}
+		return null;
+	}
+	
+	public static void main(String[] args) {
+		TestBase test = new TestBase();
+		System.out.println(test.getBrowser("chrome"));
 	}
 }
