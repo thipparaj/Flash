9029cd2a6c19d423593b4feb9086dbb633ec073b
thipparaj <thipparaj@gmail.com>
2018-06-16 03:22:51 +0530

    driver folder update

---
+++ practice/src/main/java/testScript/practice/testBase/TestBase.java
@@ -0,0 +1,13 @@
+package testScript.practice.testBase;
+
+import org.openqa.selenium.WebDriver;
+
+public class TestBase {
+	
+	public WebDriver driver;
+	
+
+	public void getBrowser(String myBrowser) {
+		
+	}
+}
